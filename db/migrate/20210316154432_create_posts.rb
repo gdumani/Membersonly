3f53ec422528ca31bc19309cc9ac51501c5533ae
gdumani <gdumani@gmail.com>
2021-03-16 13:43:05 -0600

    fixed posts being displaye after posted

--- db/migrate/20210316154432_create_posts.rb
+++ db/migrate/20210316154432_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      t.string :user_id
+      t.belongs_to :user, null: false, foreign_key: true
       t.text :body
       t.datetime :publish_at
 
