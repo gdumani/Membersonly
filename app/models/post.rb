3d4f1a988332837a731fadd070d85fd88ef62a79
mina <takhminam@gmail.com>
2021-03-17 01:10:11 +0300

    Add validation to the post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,11 @@
 class Post < ApplicationRecord
-    belongs_to :user
+    belongs_to :user 
+
+validates :body, length: { minimum: 1, maximum: 280 }
+validates :publish_at, presence: true
+
+after_initialize do
+  self.publish_at ||= 24.hours.from_now 
+end 
 end
 
