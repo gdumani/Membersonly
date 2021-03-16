81ae366ecaf0d64d3daa17d9dfbd9a5db20e073a
gdumani <gdumani@gmail.com>
2021-03-16 16:07:02 -0600

    created shared post for main index page. Not working yet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_16_154432) do
+ActiveRecord::Schema.define(version: 2021_03_16_201154) do
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2021_03_16_154432) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
